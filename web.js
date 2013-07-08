2bc50c9ca6abb73862e4d4cc52953a45ebc22f0e
esrobinson <e.s.robinson@gmail.com>
2013-07-08 02:11:17 +0000

    Should read form index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buff = fs.readFileSync('index.html');
+var out = buff.toString()
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
